994816851c7f0d429d13d6d7bbc767c4855845d7
Ro <lichtseeker@gmail.com>
2023-08-19 12:54:30 -0700

    Make a scroll view as the root element

--- app/src/main/java/com/zelda/thehungrydeveloper/MainActivity.java
+++ app/src/main/java/com/zelda/thehungrydeveloper/MainActivity.java
@@ -1,14 +1,21 @@
 package com.zelda.thehungrydeveloper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    CardView startersCard;
+    CardView mainsCard;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startersCard = findViewById(R.id.card_view_starters);
+        mainsCard = findViewById(R.id.card_view_mains);
     }
 }
