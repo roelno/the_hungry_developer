2fcf588a9af042d0604e7dea6cee4702e2496fdd
Ro <lichtseeker@gmail.com>
2023-08-20 08:51:55 -0700

    Intents Moving to a new Activity

--- app/src/main/java/com/zelda/thehungrydeveloper/MainActivity.java
+++ app/src/main/java/com/zelda/thehungrydeveloper/MainActivity.java
@@ -3,7 +3,9 @@ package com.zelda.thehungrydeveloper;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +19,13 @@ public class MainActivity extends AppCompatActivity {
 
         startersCard = findViewById(R.id.card_view_starters);
         mainsCard = findViewById(R.id.card_view_mains);
+
+        startersCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent starterActivityIntent = new Intent(MainActivity.this, StartersActivity.class);
+                startActivity(starterActivityIntent);
+            }
+        });
     }
 }
